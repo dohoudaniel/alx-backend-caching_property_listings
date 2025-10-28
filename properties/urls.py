2db9b9363345347e726adf7356b282e59faf3523
dohoudaniel <dohoudanielfavour@gmail.com>
2025-10-28 13:26:25 +0100

    fix: script

--- properties/urls.py
+++ properties/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = "properties"
+
 urlpatterns = [
     path("properties/", views.property_list, name="property-list"),
 ]
--- properties/views.py
+++ properties/views.py
@@ -8,8 +8,12 @@ from .models import Property
 
 @cache_page(60 * 15)  # Cache for 15 minutes
 def property_list(request):
-    properties = Property.objects.all().values(
+    """
+    Returns all properties as JSON with a top-level "data" key.
+    Cached in Redis via settings.CACHES.
+    """
+    qs = Property.objects.all().values(
         "id", "title", "description", "price", "location", "created_at"
     )
-    return JsonResponse(list(properties), safe=False)
-
+    data = list(qs)
+    return JsonResponse({"data": data})
